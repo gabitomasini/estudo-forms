dc0660bc7e57aea55c69ea96ddcabe6256c2fb64
gabitomasini <gabriela_tomasini@hotmail.com>
2022-05-01 01:27:08 -0300

    Estudo sobre Template Driven Form (Angular)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { TemplateFormModule } from './template-form/template-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template-form/template-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: null,
+    email: null
+  }
+
+  onSubmit(form: any){
+    console.log(form);
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
